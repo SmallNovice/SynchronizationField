e4a6b7e4695d2fcad599984d6f1014f3a55f4f31
SmallNovice <691513006@qq.com>
2021-10-16 00:43:15 +0800

    create form table modify

--- app/controllers/initial_field_to_form_fields_controller.rb
+++ app/controllers/initial_field_to_form_fields_controller.rb
@@ -3,7 +3,7 @@ class InitialFieldToFormFieldsController < ApplicationController
   before_action :get_fields
 
   def receive
-    form_table_name = FormTableName.table_name(params['form']['namespace_id'], params['form']['id'])
+    form_table_name = FormTableName.table_name(params[:form][:namespace_id], params[:form][:id])
     CreateFormTableJob.perform_now(form_table_name, @fields_sets)
     # CreateFormFieldsJob.perform_now(FormTableName.table_name, @fields_sets)
   end
